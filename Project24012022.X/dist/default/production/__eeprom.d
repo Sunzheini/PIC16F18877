bb747366ba46aa0a116cc6cd20afc9b226e38e72
Sunzheini <daniel_zorov@abv.bg>
2022-06-26 19:51:29 +0300

    Update2

--- Project24012022.X/dist/default/production/__eeprom.d
+++
@@ -1,2 +0,0 @@
-dist/default/production\__eeprom.p1: \
-  D:\Study\Microchip\xc8\v2.32\pic\sources\c99\pic\__eeprom.c
--- TemplateAuto.X/build/default/production/lcd_library.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/lcd_library.p1:  \
-lcd_library.c  \
-lcd_library.h 
